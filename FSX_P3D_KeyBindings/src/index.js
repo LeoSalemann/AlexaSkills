16030068fa652368e10d133341d0aa804678f4ad
LeoSalemann <leo.salemann@me.com>
2016-10-26 19:03:32 -0700

    This works
    clean upload.

--- FSX_P3D_KeyBindings/src/index.js
+++ FSX_P3D_KeyBindings/src/index.js
@@ -15,13 +15,32 @@ exports.handler = function(event, context, callback) {
 var handlers = {
     //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
     //Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.emit('RecipeIntent');
+    },
+
     'NewSession': function () {
+//        this.emit('RecipeIntent');
+/*
+      var itemSlot = this.event.request.intent.slots.Item;
+      var itemName;
+      if (itemSlot && itemSlot.value) {
+          var recipe = recipes[itemName];
+
+          itemName = itemSlot.value.toLowerCase();
+
+          this.attributes['speechOutput'] = "I have item " + itemName + " and recipe " + recipe;
+
+      } else {
+*/
         this.attributes['speechOutput'] = 'Welcome to ' + SKILL_NAME + '. You can ask a question like,' +
+            ' how do I lower my flaps? And I\'ll answer with something like F7. ' +
             'Now, what can I help you with?';
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
+/*      } */
         this.attributes['repromptSpeech'] = 'For instructions on what you can say, please say help me.';
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
     'RecipeIntent': function () {
         var itemSlot = this.event.request.intent.slots.Item;
@@ -34,6 +53,7 @@ var handlers = {
         var recipe = recipes[itemName];
 
         if (recipe) {
+            this.attributes['speechOutput'] = recipe + " will " + itemName + " ... What else can I help with?";
             this.attributes['repromptSpeech'] = 'Try saying repeat.';
             this.emit(':askWithCard', this.attributes['speechOutput'] , this.attributes['repromptSpeech'], cardTitle, recipe);
         } else {
@@ -53,8 +73,8 @@ var handlers = {
         }
     },
     'AMAZON.HelpIntent': function () {
-        this.attributes['speechOutput'] = 'You can ask questions such as, what\'s the key binding for lowering flaps, or, you can say exit... ' +
-            'Now, what can I help you with?';
+        this.attributes['speechOutput'] = 'You can ask questions such as, what\'s the key binding for lowering flaps, ' +
+        ' And I\'ll respond with somethlink like F7. or, you can say exit... Now, what can I help you with?';
         this.attributes['repromptSpeech'] = 'You can say things like, what\'s the key binding for lowering flaps, or you can say exit...' +
             ' Now, what can I help you with?';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
